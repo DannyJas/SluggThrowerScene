5373389100540014c3b8931e0dfc26cdd5ec4345
Daniel Jasinski <djas1229@gmail.com>
2021-04-14 14:07:41 -0500

    More work

---
+++ Assets/EnemyScript.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class EnemyScript : MonoBehaviour
+{
+
+    public NavMeshAgent AIPreFab;
+
+    public Transform goal; 
+    public float interval = 2;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(SpawnAI());
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    IEnumerator SpawnAI() {
+    while(true) {
+        yield return new WaitForSeconds(interval); 
+        MoveTo copy = Instantiate(AIPreFab, this.transform.position, this.transform.rotation);
+        copy.goal = goal;
+    }
+}
+
+}
