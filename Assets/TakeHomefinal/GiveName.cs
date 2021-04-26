0118f0f40387eb258f8af2959d23d7f3497f109f
Daniel Jasinski <djas1229@gmail.com>
2021-04-26 03:03:55 -0500

    Finalwork part1

---
+++ Assets/TakeHomefinal/GiveName.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class GiveName : MonoBehaviour
+{
+    TMP_Text RandomName; 
+    string [] names = new string [30];
+    // Start is called before the first frame update
+    void Start()
+    {
+       names[0] = "Bloodspiller";  
+       names[1] = "Harmony";
+       names[2] = "Remorse";
+       names[3] = "Singed Hand Cannon";
+       names[4] = "Proud Shooter";
+       names[5] = "Bandit's Golden Six-Shooter";
+       names[6] = "Furious Golden Hand Cannon";
+       names[7] = "Last Chance";
+       names[8] = "Gift Of Grieving Widows";
+       names[9] = "Finality";
+       names[10] = "Crusader Of Broken Dreams";
+       names[11] = "Mercy";
+       names[12] = "Might Of The Beast";
+       names[13] = "Warbringer";
+       names[14] = "Agony";
+       names[15] = "Spiteful Gun";
+       names[16] = "Private Six-Shooter";
+       names[17] = "Stormfury Ebon Flintlock";
+       names[18] = "Conqueror's Ironbark Flintlock";
+       names[19] = "Queller Of The Occult";
+       names[20] = "Daddy";
+       names[21] = "Dominion";
+       names[22] = "Contra";
+       names[23] = "Ultra85";
+       names[24] = "Iron man";
+       names[25] = "Ex-wife";
+       names[26] = "Captain";
+       names[27] = "Logic";
+       names[28] = "Stick";
+       names[29] = "Happy";
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/TakeHomefinal/scripts/WeaponSpawner.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WeaponSpawner : MonoBehaviour
+{
+    [SerializeField] 
+    GameObject EarthBlaster, FireBlaster, AirBlaster, WaterBlaster;
+
+    [SerializeField]
+    Transform BlasterSpawnPoint; 
+ 
+
+     GameObject[] Blasters = new GameObject [4]; 
+
+     bool canSpawn = true;
+
+ 
+    GameObject Blaster;
+
+    bool canspawn;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Blasters[0] = EarthBlaster; 
+        Blasters[1] = FireBlaster; 
+        Blasters[2] = AirBlaster;
+        Blasters[3] = WaterBlaster;
+    }
+
+    // Update is called once per frame
+
+    void OnTriggerEnter(Collider other) {
+        if(other.gameObject.CompareTag("Player")){
+            canSpawn = true;
+            if(canSpawn) {
+             int RandomBlaster = Random.Range(0,4);
+              Blaster = Instantiate(Blasters[RandomBlaster].gameObject, BlasterSpawnPoint.position, BlasterSpawnPoint.rotation, null);
+              canSpawn = false;
+            }
+        }
+    }
+}
